651397b8131c42926def3218aeec49949b18edd5
felix1805 <felix1805@gmail.com>
2023-07-07 16:50:30 -0400

    created schedule card, started styling, linked with slug.js

--- components/ScheduleCard.js
+++ components/ScheduleCard.js
@@ -15,10 +15,21 @@ const myWixClient = createClient({
 const ScheduleCard = ({ availabilityEntry }) => {
   const startDate = moment(availabilityEntry.slot.startDate, 'YYYY-MM-DD HH:mm:ss')
   const startDay = startDate.format('ddd MM yyyy')
-  return (
-    <button>
-      <p>{startDay}</p>
+  const startTime = startDate.format('HH:mm')
+  const endTime = moment(availabilityEntry.slot.endDate, 'YYYY-MM-DD HH:mm:ss').format('HH:mm')
 
+  const createRedirect = async (slotAvailability) => {
+    const redirect = await myWixClient.redirects.createRedirectSession({
+      bookingsCheckout: { slotAvailability, timezone: 'EST' },
+      callbacks: { postFlowUrl: window.location.href }
+    })
+    window.location = redirect.redirectSession.fullUrl
+  }
+  return (
+    <button className="schedule-card-button" onClick={() => createRedirect(availabilityEntry)}>
+      <p>{startDay}</p>
+      <p>{startTime}</p>
+      <p>{endTime}</p>
     </button>
   )
 }
